cee1da7d279235f02cbb3105679ae6ee3cc832ac
Hao Liu <liuhaodeu@gmail.com>
2023-10-20 18:40:33 +0200

    feat: finished H3

--- src/main/java/h01/GameController.java
+++ src/main/java/h01/GameController.java
@@ -1,7 +1,9 @@
 package h01;
 
 import fopbot.Robot;
+import fopbot.World;
 import h01.template.GameControllerBase;
+import h01.template.Utils;
 import org.tudalgo.algoutils.student.Student;
 
 /**
@@ -18,7 +20,32 @@ public class GameController extends GameControllerBase {
 
     @Override
     public void checkWinCondition() {
-        // TODO: H3
-        Student.crash("H3 - remove if implemented");
+        // cleaner wins if both contaminants turned off or at least 200 coins in the dump zone
+        boolean isCleanerWin = (getContaminant1().isTurnedOff() && getContaminant2().isTurnedOff())
+            || (Utils.getCoinAmount(0, World.getHeight() - 1) >= 200);
+        // calculate the amount of total fields and fields with coins
+        int totalFieldAmount = World.getHeight() * World.getWidth();
+        int coinsFieldAmount = 0;
+        for (int i = 0; i < World.getWidth(); i++) {
+            for (int j = 0; j < World.getHeight(); j++) {
+                if (Utils.getCoinAmount(i, j) > 0) {
+                    coinsFieldAmount++;
+                }
+            }
+        }
+        // winning condition for contaminants
+        boolean isContaminantsWin = 2 * coinsFieldAmount >= totalFieldAmount;
+        if(isCleanerWin||isContaminantsWin){
+            cleaningRobot.turnOff();
+            contaminant1.turnOff();
+            contaminant2.turnOff();
+            // announce the winner
+            if (isCleanerWin){
+                System.out.println("CLeaning robot won!");
+            } else {
+                System.out.println("Contaminants won!");
+            }
+            stopGame();
+        }
     }
 }
