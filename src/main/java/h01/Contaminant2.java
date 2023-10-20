ff1914eccaaab10e8b2c13b3a8c6c0e92b14b2a9
Hao Liu <liuhaodeu@gmail.com>
2023-10-20 18:08:05 +0200

    feat: finished H2.2

--- src/main/java/h01/Contaminant2.java
+++ src/main/java/h01/Contaminant2.java
@@ -5,7 +5,10 @@ import fopbot.Robot;
 import fopbot.RobotFamily;
 import h01.template.Contaminant;
 import h01.template.TickBased;
+import h01.template.Utils;
 import org.tudalgo.algoutils.student.Student;
+
+import java.util.ArrayList;
 
 /**
  * A {@link Contaminant}-{@link Robot} that moves in a predefined way and contaminates the floor.
@@ -31,7 +34,55 @@ public class Contaminant2 extends Robot implements Contaminant, TickBased {
 
     @Override
     public void doMove() {
-        // TODO: H2.2
-        Student.crash("H2.2 - remove if implemented");
+        if (!hasAnyCoins()) {
+            turnOff();
+        }
+        // if the robot is off or has no coin, the method will end
+        if (!isTurnedOff() && hasAnyCoins()) {
+            // check if 2 coins at the field
+            int coinsHere = Utils.getCoinAmount(getX(), getY());
+            if (coinsHere < 2) {
+                // put the coins
+                int putCoinsCounter = Math.min(2 - coinsHere, getNumberOfCoins());
+                for (int i = 0; i < putCoinsCounter; i++) {
+                    putCoin();
+                }
+            }
+            // record how many left turns are required in all possible directions
+            ArrayList<Integer> possibleTurns = new ArrayList<>();
+            for (int i = 0; i < 4; i++) {
+                if (isFrontClear()) {
+                    possibleTurns.add(i);
+                }
+                turnLeft();
+            }
+            /*
+            turningIndex = -1, when no possible direction;
+            when there are possible directions, filter in the order of 1,0,3,2
+             */
+            int turningIndex = -1;
+            if (possibleTurns.contains(2)) {
+                turningIndex = 2;
+            }
+            if (possibleTurns.contains(3)) {
+                turningIndex = 3;
+            }
+            if (possibleTurns.contains(0)) {
+                turningIndex = 0;
+            }
+            if (possibleTurns.contains(1)) {
+                turningIndex = 1;
+            }
+            // turn to the desired direction
+            if (turningIndex != -1) {
+                for (int i = 0; i < turningIndex; i++) {
+                    turnLeft();
+                }
+            }
+            // check once again
+            if(isFrontClear()){
+                move();
+            }
+        }
     }
 }
